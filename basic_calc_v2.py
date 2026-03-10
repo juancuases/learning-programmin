06b4814a46bce3fc070ec1bd8f5383324b508d2d
juancuases <jdcuases.7552@unicesmag.edu.co>
2026-03-10 09:08:32 -0500

    Add files via upload

---
+++ basic_calc_v2.py
@@ -0,0 +1,23 @@
+´´´
+ in  python it is not necessary or  mandatory to declare variables or constants, but you can initilize them
+´´´
+#assign or initialize variables or constanst 
+num1=0
+num2=0
+
+
+
+#inputs 
+print(please, enter the first number: "")
+num1=imput()
+print(please.enter second number: "")
+num2=imput()
+
+#processes
+add=num1+num2
+#subs=num1-num2
+#mult=num1*num2
+#div=num1/num2
+#outputs
+print("the addition is:", )
+
